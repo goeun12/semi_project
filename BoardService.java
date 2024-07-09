262ed14edb7c430ef2e067e3c795a683bbb70ee9
jungmin777 <lkarawl97@gmail.com>
2024-07-09 17:21:15 +0900

    Create BoardService.java

---
+++ BoardService.java
@@ -0,0 +1,42 @@
+package com.project.aloneBab.board.model.service;
+
+import java.util.ArrayList;
+
+import com.project.aloneBab.board.model.vo.Board;
+import com.project.aloneBab.board.model.vo.DivideSearch;
+import com.project.aloneBab.board.model.vo.Image;
+import com.project.aloneBab.board.model.vo.PageInfo;
+import com.project.aloneBab.board.model.vo.Recipe;
+
+public interface BoardService {
+
+	int getListCount(String i);
+
+	ArrayList<Board> selectBoardList(PageInfo pi, String string);
+
+	ArrayList<Recipe> selectRecipeList(Integer bId);
+
+	ArrayList<Image> selectImageList(Integer rId);
+
+	Board selectBoard(int bId, String id);
+
+	int insertImage(ArrayList<Image> iList);
+
+	int deleteBoard(int bId);
+
+	int getDivideCount(DivideSearch ds);
+
+	ArrayList<Board> selectDivideBoardList(PageInfo pi, DivideSearch ds);
+
+	int insertBoard(Board b);
+
+	int insertRecipe(Recipe recipe);
+
+	ArrayList<Board> selectRecommendBoardList(String nation);
+
+//	ArrayList<Recipe> selectDivideRecipeList(DivideSearch ds);
+//
+//	ArrayList<Image> selectDivdeImageList(DivideSearch ds);
+
+
+}
