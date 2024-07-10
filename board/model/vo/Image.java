b9e662be8e13a7c3ddca378cf976c4229c50dfa8
kimnamhee-1211 <namhee6741@naver.com>
2024-07-10 12:47:10 +0900

    Add files via upload

---
+++ board/model/vo/Image.java
@@ -0,0 +1,19 @@
+package com.project.aloneBab.board.model.vo;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Image {
+	private int imgNo;
+	private int recipeNo;
+	private String imageURL;
+	private String imageName;
+	private int titleImg;
+
+}
