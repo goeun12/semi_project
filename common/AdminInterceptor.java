b1c586b348d0184e7cda44ddc91e9388b74532dc
goeun12 <reanpb@gmail.com>
2024-07-10 16:01:06 +0900

    common 합친 것

--- common/AdminInterceptor.java
+++ common/AdminInterceptor.java
@@ -11,20 +11,20 @@ import com.project.aloneBab.member.model.vo.Member;
 public class AdminInterceptor implements HandlerInterceptor{
 
 	
-	@Override
-	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
-			throws Exception {
-		
-		HttpSession session = request.getSession();		
-		Member loginUser = (Member)session.getAttribute("loginUser");
-		
-		if(loginUser.getIsAdmin().equals("N")) {
-			throw new AllException("해당 메뉴에 대한 접근 권한이 없습니다. 해당 메뉴는 관리자만 이용할 수 있습니다.");
-		}
-		
-		
-		return HandlerInterceptor.super.preHandle(request, response, handler);
-	}
+//	@Override
+//	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+//			throws Exception {
+//		
+//		HttpSession session = request.getSession();		
+//		Member loginUser = (Member)session.getAttribute("loginUser");
+//		
+//		if(loginUser.getIsAdmin().equals("N")) {
+//			throw new AllException("해당 메뉴에 대한 접근 권한이 없습니다. 해당 메뉴는 관리자만 이용할 수 있습니다.");
+//		}
+//		
+//		
+//		return HandlerInterceptor.super.preHandle(request, response, handler);
+//	}
 	
 	
 	
