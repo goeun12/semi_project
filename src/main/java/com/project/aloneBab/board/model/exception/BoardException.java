3d7c872d512719f93ac9bdf138bd8e8fc367129d
goeun12 <reanpb@gmail.com>
2024-07-10 15:59:49 +0900

    Delete src directory

--- src/main/java/com/project/aloneBab/board/model/exception/BoardException.java
+++
@@ -1,6 +0,0 @@
-package com.project.aloneBab.board.model.exception;
-
-public class BoardException extends RuntimeException{
-	public BoardException() {}
-	public BoardException(String msg) {super(msg);}
-}
--- src/main/java/com/project/aloneBab/common/AllException.java
+++
@@ -1,7 +0,0 @@
-package com.project.aloneBab.common;
-
-public class AllException extends RuntimeException{
-
-	public AllException() {}
-	public AllException(String msg) {super(msg);}
-}
