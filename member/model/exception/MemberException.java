fda7ae6578eb81c1ddc375c6bc3ff22adb5a0162
goeun12 <reanpb@gmail.com>
2024-07-12 14:20:32 +0900

    Delete member directory

--- member/model/exception/MemberException.java
+++
@@ -1,6 +0,0 @@
-package com.project.aloneBab.member.model.exception;
-
-public class MemberException extends RuntimeException{
-	public MemberException() {}
-	public MemberException(String msg) {super(msg);}
-}
