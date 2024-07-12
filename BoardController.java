61701004ccb93e410090e9182e5bc14594538da8
jungmin777 <lkarawl97@gmail.com>
2024-07-12 10:03:39 +0900

    Update BoardController.java 7/12 10:03

--- BoardController.java
+++ BoardController.java
@@ -3,6 +3,7 @@ package com.project.aloneBab.board.controller;
 import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
 
 import javax.servlet.http.HttpServletRequest;
@@ -35,7 +36,6 @@ public class BoardController {
 	@RequestMapping("recipe.re")
 	public String recipeListView(@RequestParam(value="page", defaultValue="1") int currentPage, Model model) {
 		int listCount = bService.getListCount("레시피");
-		System.out.println(listCount);
 		PageInfo pi = Pagination.getPageInfo(currentPage, listCount, 12);
 		
 		ArrayList<Board> bList = bService.selectBoardList(pi, "레시피");
@@ -44,6 +44,29 @@ public class BoardController {
 		// null 넘기면 : 전체 조회(레시피 다가져옴)
 //		System.out.println(rList);
 		ArrayList<Image> iList = bService.selectImageList(null);
+		if(bList != null) {
+			// bList 가 들어있을 때 : iList 다 비교해봤을 때 레시피 넘버랑 하나도 맞는게 없다면 해당 boardNo 보드 삭제
+			// b & r 은 1대1 관계
+			if(rList != null) {
+				for(Recipe r : rList) {
+					int count = 0;
+					
+					for(Image i : iList) {
+						if(r.getRecipeNo() == i.getRecipeNo()) {
+							count = count+1;
+						}
+					}
+					
+					if(count==0) {
+						bService.deleteBoard(r.getBoardNo());
+					}
+					
+				}
+				bList = bService.selectBoardList(pi, "레시피");
+			}
+		}else {
+			
+		}
 		// null 넘기면 : 전체 조회(썸네일[image_level = 0] 가져옴)
 //		System.out.println(iList);
 		
@@ -61,11 +84,25 @@ public class BoardController {
 	}
 	
 	// 레시피 분류한것들 조회
-	@RequestMapping("bunryuRecipe.re")
-	public String bunryuRecipe(@ModelAttribute DivideSearch ds, Model model, @RequestParam("searchWord") String find) {
-		if(find.trim() == null && ds.getNation().equals("all") && ds.getDifficulty().equals("all")) { // 아무것도 안하고 검색 눌렀음
-			return "recipeList"; // 돌려보냄
-		}else { // 무언가 검색어를 썼거나, 나라를 선택했거나, 난이도를 선택했음 
+	@RequestMapping("searchRecipe.re")
+	public String bunryuRecipe(@RequestParam(value="nation", required=false) String nation, @RequestParam(value="difficulty", required=false) String diffi, @RequestParam(value="searchWord", required=false) String find, Model model) {
+//	public String bunryuRecipe(@ModelAttribute DivideSearch ds, Model model) {
+//		if(ds.getSearchWord().trim() == null && ds.getNation().equals("all") && ds.getDifficulty().equals("all")) { // 아무것도 안하고 검색 눌렀음
+//			return "recipeList"; // 돌려보냄
+		if(diffi==null && find == null) {
+			diffi = "all";
+			find = "";
+		}
+		System.out.println(diffi);
+		System.out.println(find);
+		if(find.trim().isEmpty() && nation.equals("all") && diffi.equals("all")) {
+			System.out.println("검색어 없고 전체 국가 난이도 없슴");
+			return "recipeList";
+		} else { // 무언가 검색어를 썼거나, 나라를 선택했거나, 난이도를 선택했음 
+			DivideSearch ds = new DivideSearch();
+			ds.setDifficulty(diffi);
+			ds.setNation(nation);
+			ds.setSearchWord(find);
 			int listCount1 = bService.getDivideCount(ds); // 단어, 나라, 난이도로 분류한 갯수
 			PageInfo pi = Pagination.getPageInfo(1, listCount1, 12);
 			
@@ -88,6 +125,7 @@ public class BoardController {
 				model.addAttribute("nothing", nothing);
 				return "recipeList";
 			}
+		
 		}
 	}
 	
@@ -95,36 +133,37 @@ public class BoardController {
 	// 레시피 상세 보기
 	@RequestMapping("recipeContent.re")
 	public String recipeContentView(@RequestParam("bNo") int bNo, @RequestParam("page") int page, @RequestParam("rNo") int rNo,  HttpSession session, Model model) {
-		System.out.println("레시피 상세보기");
-		Member loginUser = (Member)session.getAttribute("loginUser");
+		//Member loginUser = (Member)session.getAttribute("loginUser");
 		String id = null;
-		if(loginUser != null) {
-			id = loginUser.getId();
-			System.out.println("로그인 유저 있음");
-		}
-		Board b = bService.selectBoard(bNo, id);// bId 는 보드넘버, id 는 로그인유저 아이디
-		ArrayList<Recipe> r = bService.selectRecipeList((Integer)bNo);// 선택한 레시피 가져오기
-		ArrayList<Image> iList = bService.selectImageList((Integer)rNo);// 레시피번호로 이미지들 가져오기
+		if((Member)session.getAttribute("loginUser") != null) {
+			id = ((Member)session.getAttribute("loginUser")).getId();
+			
+		} 
+			
+			Board b = bService.selectBoard(bNo, id);// bId 는 보드넘버, id 는 로그인유저 아이디
+			ArrayList<Recipe> r = bService.selectRecipeList((Integer)bNo);// 선택한 레시피 가져오기
+			ArrayList<Image> iList = bService.selectImageList((Integer)rNo);// 레시피번호로 이미지들 가져오기
+			
+			if(b != null) {
+				String[] contents = b.getContent().split("§§●");
+				ArrayList<Board> bList = bService.selectRecommendBoardList(r.get(0).getNation()); // 같은 국가의 요리들 목록
+				ArrayList<Recipe> rList = bService.selectRecipeList(null); // 전체 레시피 리스트
+				ArrayList<Image> iListAll = bService.selectImageList(null); // 썸네일 사진 목록
+				Collections.shuffle(bList);
+				
+				model.addAttribute("b",b);
+				model.addAttribute("r",r);
+				model.addAttribute("iList", iList);
+				model.addAttribute("contents", contents);
+				model.addAttribute("rList", rList);
+				model.addAttribute("iListAll", iListAll);
+				model.addAttribute("bList", bList);
+				
+				return "recipeContent";
+			} else {
+				return "에러페이지";
+			}
 		
-		if(b != null) {
-			String[] contents = b.getContent().split("§§●");
-			System.out.println(contents[0]);
-			ArrayList<Board> bList = bService.selectRecommendBoardList(r.get(0).getNation());
-			ArrayList<Recipe> rList = bService.selectRecipeList(null);
-			ArrayList<Image> iListAll = bService.selectImageList(null);
-			
-			model.addAttribute("b",b);
-			model.addAttribute("r",r);
-			model.addAttribute("iList", iList);
-			model.addAttribute("contents", contents); // model addattribute 이거 하나 더 했떠니 오류 생김 + 110줄에 String[] 배열 만들고서 애드함
-			model.addAttribute("rList", rList);
-			model.addAttribute("iListAll", iListAll);
-			model.addAttribute("bList", bList);
-			
-			return "recipeContent";
-		} else {
-			return "에러페이지";
-		}
 	}
 	
 	// 레시피 작성 페이지 이동
@@ -143,7 +182,6 @@ public class BoardController {
 		b.setTitle(title);
 		b.setContent(content);
 		
-		System.out.println(content);
 		
 		ArrayList<Image> iList = new ArrayList<Image>();
 		for(int i = 0; i< files.size(); i++) {
@@ -167,29 +205,23 @@ public class BoardController {
 			} else {
 				img.setTitleImg(1);
 			}
-			System.out.println("이미지 수 : "+i);
 		}
 		
 		int result1 = 0;
 		int result2 = 0;
-		System.out.println(iList);
 		if(iList.isEmpty()) {
 			// 이미지가 없다는 뜻
 			// 이미지 안넣냐고 물어보기
 			System.out.println("이미지 리스트 없음");
 		} else {
-			System.out.println("insertBoard 전");
 			result1 = bService.insertBoard(b); // 게시판에 넣는 서비스 성공시 1
-			System.out.println("161 : "+result1);
 			if(result1>0) {
 				int rResult = bService.insertRecipe(recipe);
-				System.out.println("164 : "+rResult);
 				if(rResult>0) {
 					for(Image i : iList) {
 						i.setRecipeNo(recipe.getRecipeNo());
 					}
 					result2 = bService.insertImage(iList); // 성공시 이미지 갯수만큼 n
-					System.out.println("170 : "+result2);
 				}else {
 					return "에러페이지";
 				}
@@ -215,7 +247,6 @@ public class BoardController {
 	// 이미지 저장 메소드
 	public String[] saveFile(MultipartFile upload, HttpServletRequest req) {
 		String savePath = req.getSession().getServletContext().getRealPath("resources") + "\\image";
-		System.out.println(savePath);
 		
 		File folder = new File(savePath);
 		if(!folder.exists()) folder.mkdirs(); // image 폴더 생성
@@ -228,7 +259,7 @@ public class BoardController {
 		try {
 			upload.transferTo(new File(renamePath));
 		} catch (Exception e) {
-			System.out.println("파일 전송 실패" + e.getMessage());
+			System.out.println("파일 전송 실패" + e.getMessage()); // throw exception
 		}
 		String[] returnArr = new String[2];
 		returnArr[0] = savePath;
@@ -247,7 +278,7 @@ public class BoardController {
 		}
 	}
 	
-	@RequestMapping("updateRecipe.re")
+	@RequestMapping("updateRecipe.re") // 수정페이지 들어가기
 	public String updateRecipe(@RequestParam("bNumber") int bNo, @RequestParam("rNumber") int rNo, Model model) {
 		Board b = bService.selectBoard(bNo, null);// bId 는 보드넘버, id 는 로그인유저 아이디
 		ArrayList<Recipe> r = bService.selectRecipeList((Integer)bNo);// 선택한 레시피 가져오기
@@ -267,20 +298,93 @@ public class BoardController {
 		}
 	}
 	
-	@RequestMapping("editRecipe.re")
-	public String editRecipe(@RequestParam("title") String title, @RequestParam("content") String content, @ModelAttribute Recipe recipe, @RequestParam("file") ArrayList<MultipartFile> files, HttpServletRequest req) {
+	@RequestMapping("editRecipe.re") // 수정한거 집어넣기
+	public String editRecipe(@RequestParam("title") String title, @RequestParam("content") String content, @ModelAttribute Recipe recipe, @RequestParam("file") ArrayList<MultipartFile> files, HttpServletRequest req, Model model) {
 		String id = ((Member)req.getSession().getAttribute("loginUser")).getId();
 		Board b = new Board();
+		b.setBoardNo(recipe.getBoardNo());
 		b.setWriter(id);
 		b.setBoardGenre("레시피");
 		b.setTitle(title);
 		b.setContent(content);
 	
+		ArrayList<Image> iList = new ArrayList<Image>();
+		for(int i = 0; i< files.size(); i++) {
+			MultipartFile upload = files.get(i);
+			if(upload.getOriginalFilename() != "") {
+				String[] returnArr = saveFile(upload, req);
+				
+				Image img = new Image();
+				img.setRecipeNo(recipe.getRecipeNo());
+				img.setImageName(returnArr[1]);
+				img.setImageURL(returnArr[0]);
+				iList.add(img);
+			} else {
+				return "에러페이지"; // 뭔가 에러 아무튼 돌려보내면 됨
+			}
+		}
 		
+		for(int i=0; i<iList.size(); i++) {
+			Image img = iList.get(i);
+			if(i==0) {
+				img.setTitleImg(0);
+			} else {
+				img.setTitleImg(1);
+			}
+		}
 		
-		
-		
-		return null;
+		int result1 = 0;
+		int result2 = 0;
+		if(iList.isEmpty()) {
+			// 이미지 하나라도 비어있으면 못 넘어오게 바꿈 = 여기 절대 못지나감
+			return "redirect:recipeList.jsp";
+		} else {
+			result1 = bService.updateBoard(b); // 게시판에 넣는 서비스 성공시 1
+			if(result1>0) {
+				int rResult = bService.updateRecipe(recipe);
+				if(rResult>0) {
+					for(Image i : iList) {
+						i.setRecipeNo(recipe.getRecipeNo());
+					}
+					bService.deleteImage(recipe.getRecipeNo()); // 기존 이미지 다 삭제
+					result2 = bService.editImage(iList); // 성공시 이미지 갯수만큼 n
+				}else {
+					return "에러페이지";
+				}
+			}else {
+				return "에러페이지";
+			}
+			
+			// return 없음 313줄
+		} // if(iList.isEmpty()) 문 탈출
+		if(result1 + result2 == 1 + iList.size()) { // 잘 들어갔다는 뜻
+			Board b2 = bService.selectBoard(recipe.getBoardNo(), id);// bId 는 보드넘버, id 는 로그인유저 아이디
+			ArrayList<Recipe> r = bService.selectRecipeList(recipe.getBoardNo());// 선택한 레시피 가져오기
+			ArrayList<Image> iList2 = bService.selectImageList(recipe.getRecipeNo());// 레시피번호로 이미지들 가져오기
+			
+			if(b != null) {
+				String[] contents = b.getContent().split("§§●");
+				ArrayList<Board> bList = bService.selectRecommendBoardList(r.get(0).getNation());
+				ArrayList<Recipe> rList = bService.selectRecipeList(null);
+				ArrayList<Image> iListAll = bService.selectImageList(null);
+				
+				model.addAttribute("b",b2);
+				model.addAttribute("r",r);
+				model.addAttribute("iList", iList2);
+				model.addAttribute("contents", contents); // model addattribute 이거 하나 더 했떠니 오류 생김 + 110줄에 String[] 배열 만들고서 애드함
+				model.addAttribute("rList", rList);
+				model.addAttribute("iListAll", iListAll);
+				model.addAttribute("bList", bList);
+				
+				return "recipeContent";
+			}
+		} else { // 에러가 나서 레시피 작성에 실패한 경우
+			for(Image i : iList) {
+				deleteFile(i.getImageName(), req);
+			}
+			return "redirect:index.jsp";
+		}
+		return "redirect:recipeList.jsp";
 	}	
 		
 	@RequestMapping("deleteRecipe.re")
